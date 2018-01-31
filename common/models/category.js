e219dfe7a50a333491f3939ef5c7ccfcf2ef0b14
Imre Bartis <imre.bartis@siili.com>
2018-01-31 09:08:32 +0200

    creating a relation btw 2 LoopBack models

---
+++ common/models/category.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = function(Category) {
+
+};
+//TERMINAL:
+// lb property
+// ? Select the model: Product
+// ? Enter the property name: categoryId
+// ? Property type: number
+// ? Required? No
+// ? Default value[leave blank for none]: 
+
+// lb relation
+// ? Select the model to create the relationship from: Category
+// ? Relation type: has many
+// ? Choose a model to create a relationship with: Product
+// ? Enter the property name for the relation: products
+// ? Optionally enter a custom foreign key: categoryId
+// ? Require a through model? No
+// ? Allow the relation to be nested in REST APIs: No
+// ? Disable the relation from being included: Yes
+
+// lb relation
+// ? Select the model to create the relationship from: Product
+// ? Relation type: belongs to
+// ? Choose a model to create a relationship with: Category
+// ? Enter the property name for the relation: category
+// ? Optionally enter a custom foreign key: categoryId
+// ? Allow the relation to be nested in REST APIs: No
+// ? Disable the relation from being included: Yes
+
+
+
